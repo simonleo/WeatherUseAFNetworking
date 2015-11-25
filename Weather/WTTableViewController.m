b3007c450b5985df59aa754d3c4d1181b8fc348b
liushu <liushu740010494@126.com>
2015-11-25 19:41:49 +0800

    setImage

--- Weather/WTTableViewController.m
+++ Weather/WTTableViewController.m
@@ -12,6 +12,7 @@
 #import "WeatherAnimationViewController.h"
 #import "NSDictionary+weather.h"
 #import "NSDictionary+weather_package.h"
+#import "UIImageView+AFNetworking.h"
 
 static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/weather_sample/";
 
@@ -219,6 +220,19 @@ static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/wea
     }
     
     cell.textLabel.text = [daysWeather weatherDescription];
+    
+    NSURL *url = [NSURL URLWithString:daysWeather.weatherIconURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    UIImage *placeholderImage = [UIImage imageNamed:@"placeholder"];
+    
+    __weak UITableViewCell *weakCell = cell;
+    
+    [cell.imageView setImageWithURLRequest:request placeholderImage:placeholderImage success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        
+        weakCell.imageView.image = image;
+        [weakCell setNeedsLayout];
+        
+    } failure:nil];
     
     return cell;
 }
